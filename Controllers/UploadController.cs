b4f2250adb8a0392802f4ff00ea622b30e896c86
SRIJA <srija.chakraborty@gds.ey.com>
2026-01-16 09:50:41 +0530

    change

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureStorageDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public UploadController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
