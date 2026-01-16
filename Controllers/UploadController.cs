b9d0eefae3a47a6cc9c89fea72f339924c4dd4f7
SRIJA <srija.chakraborty@gds.ey.com>
2026-01-16 10:06:11 +0530

    change

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AzureStorageDemo.Controllers
@@ -11,6 +12,23 @@ namespace AzureStorageDemo.Controllers
         public UploadController(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            //Read the configuration
+            var cnstring = _configuration["BlobStorage:ConnectionString"];
+            var containername = _configuration["BlobStorage:ContainerName"];
+
+            //Connect to Container
+            var containerClient = new BlobContainerClient(cnstring, containername);
+            //Create blob file
+            var blobclient = containerClient.GetBlobClient(file.FileName);
+            //Upload file
+            using var stream = file.OpenReadStream();
+            await blobclient.UploadAsync(stream, overwrite: true);
+            return Ok("file uploaded successfully..");
+
         }
     }
 }
